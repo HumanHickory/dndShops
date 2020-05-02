079698fbbdd12d98af37e64e243de0c05fd4d0df
HumanHickory <34848650+HumanHickory@users.noreply.github.com>
2020-05-02 18:59:07 -0400

    Add files via upload

---
+++ shops/js/ricochet.js
@@ -0,0 +1,61 @@
+ function Sonridge() {
+
+            var content1 = '<div class="img-holder">';
+            content1 += '<img src="../img/Armor/breastplateGirl.png"  id="img1">';
+            content1 += '<div class="hidden text-center" id="hidden1">';
+            content1 += '<ul>';
+            content1 += '<li><b>AC:</b> 14 + Dex</li>';
+            content1 += '<li>(max dex = 2)</li>';
+            content1 += '<li>Light Armor</li>';
+            content1 += '<li></li><li></li></ul></div></div>';
+            content1 += '<h5> Aryansport Breastplate </h5>';
+            content1 += '<p> 450gp </p>';
+
+            $("#1").html(content1);
+
+            var content2 = '<div class="img-holder">';
+            content2 += '<img src="../img/Armor/fullplateMan.png" id="img2">';
+            content2 += '<div class="hidden text-center" id="hidden2">';
+            content2 += '<ul>';
+            content2 += '<li><b>AC:</b> 18</li>';
+            content2 += '<li>Heavy Armor</li>';
+            content2 += '<li><b>Stealth Disadvantage!</b></li>';
+            content2 += '<li>Strength Requirement: 15</li>';
+            content2 += '<li>Resistance to nacrotic damage</li>';
+            content2 += '</ul></div></div>';
+            content2 += '<h5> Elven Plate Armor </h5>';
+            content2 += '<p> 1,600gp</p>';
+
+            $("#2").html(content2);
+
+        }
+
+        function DefaultExclusives() {
+                        var content1 = '<div class="img-holder">';
+            content1 += '<img src="../img/Armor/breastplateGirl.png"  id="img1">';
+            content1 += '<div class="hidden text-center" id="hidden1">';
+            content1 += '<ul>';
+            content1 += '<li><b>AC:</b> 14 + Dex</li>';
+            content1 += '<li>(max dex = 2)</li>';
+            content1 += '<li>Light Armor</li>';
+            content1 += '<li></li><li></li></ul></div></div>';
+            content1 += '<h5> Aryansport Breastplate </h5>';
+            content1 += '<p> 450gp </p>';
+
+            $("#1").html(content1);
+
+            var content2 = '<div class="img-holder">';
+            content2 += '<img src="../img/Armor/fullplateMan.png" id="img2">';
+            content2 += '<div class="hidden text-center" id="hidden2">';
+            content2 += '<ul>';
+            content2 += '<li><b>AC:</b> 18</li>';
+            content2 += '<li>Heavy Armor</li>';
+            content2 += '<li><b>Stealth Disadvantage!</b></li>';
+            content2 += '<li>Strength Requirement: 15</li>';
+            content2 += '<li>Resistance to nacrotic damage</li>';
+            content2 += '</ul></div></div>';
+            content2 += '<h5> Elven Plate Armor </h5>';
+            content2 += '<p> 1,600gp</p>';
+
+            $("#2").html(content2);
+        }

bb08c6cb123d1d1ccc149c5c43a2a0582bba0167
HumanHickory <34848650+HumanHickory@users.noreply.github.com>
2021-09-24 13:04:26 -0400

    Add files via upload

---
+++ shops/js/ricochet.js
@@ -0,0 +1,108 @@
+function Sonridge() {
+
+    var content1 = '<div class="img-holder">';
+    content1 += '<img src="../img/Armor/breastplateGirl.png"  id="img1">';
+    content1 += '<div class="hidden text-center" id="hidden1">';
+    content1 += '<ul>';
+    content1 += '<li><b>AC:</b> 14 + Dex</li>';
+    content1 += '<li>(max dex = 2)</li>';
+    content1 += '<li>Light Armor</li>';
+    content1 += '<li></li><li></li></ul></div></div>';
+    content1 += '<h5>TBD </h5>';
+    content1 += '<p> 450gp </p>';
+
+    $("#1").html(content1);
+
+    var content2 = '<div class="img-holder">';
+    content2 += '<img src="../img/Armor/fullplateMan.png" id="img2">';
+    content2 += '<div class="hidden text-center" id="hidden2">';
+    content2 += '<ul>';
+    content2 += '<li><b>AC:</b> 18</li>';
+    content2 += '<li>Heavy Armor</li>';
+    content2 += '<li><b>Stealth Disadvantage!</b></li>';
+    content2 += '<li>Strength Requirement: 15</li>';
+    content2 += '<li>Resistance to nacrotic damage</li>';
+    content2 += '</ul></div></div>';
+    content2 += '<h5> TBD</h5>';
+    content2 += '<p> 1,600gp</p>';
+
+    $("#2").html(content2);
+
+}
+
+function Arynsport() {
+    var content1 = '<div class="img-holder">';
+    content1 += '<img src="../img/Armor/breastplateGirl.png"  id="img1">';
+    content1 += '<div class="hidden text-center" id="hidden1">';
+    content1 += '<ul>';
+    content1 += '<li><b>AC:</b> 14 + Dex</li>';
+    content1 += '<li>(max dex = 2)</li>';
+    content1 += '<li>Light Armor</li>';
+    content1 += '<li></li><li></li></ul></div></div>';
+    content1 += '<h5> Arynsport Breastplate </h5>';
+    content1 += '<p> 450gp </p>';
+
+    $("#1").html(content1);
+
+    var content2 = '<div class="img-holder">';
+    content2 += '<img src="../img/Armor/fullplateMan.png" id="img2">';
+    content2 += '<div class="hidden text-center" id="hidden2">';
+    content2 += '<ul>';
+    content2 += '<li><b>AC:</b> 18</li>';
+    content2 += '<li>Heavy Armor</li>';
+    content2 += '<li><b>Stealth Disadvantage!</b></li>';
+    content2 += '<li>Strength Requirement: 15</li>';
+    content2 += '<li>Resistance to fire damage</li>';
+    content2 += '</ul></div></div>';
+    content2 += '<h5> Coastal Plate Armor </h5>';
+    content2 += '<p> 1,600gp</p>';
+
+    $("#2").html(content2);
+
+
+    var content3 = '<div class="img-holder">';
+    content3 += '<img src="../img/Armor/SailorPirate.png" id="img3">';
+    content3 += '<div class="hidden text-center" id="hidden3">';
+    content3 += '<ul>';
+    content3 += '<li><b>AC:</b> AC 11 + dex</li>';
+    content3 += '<li>Light Armor</li>';
+    content3 += '<li><b>Dexterity Advantage</b></li>';
+    content3 += '</ul></div></div>';
+    content3 += '<h5> Sailor Uniform (colors and fit customizable) </h5>';
+    content3 += '<p> 10gp</p>';
+
+    $("#3").html(content3);
+
+}
+
+
+
+function DefaultExclusives() {
+    var content1 = '<div class="img-holder">';
+    content1 += '<img src="../img/Armor/breastplateGirl.png"  id="img1">';
+    content1 += '<div class="hidden text-center" id="hidden1">';
+    content1 += '<ul>';
+    content1 += '<li><b>AC:</b> 14 + Dex</li>';
+    content1 += '<li>(max dex = 2)</li>';
+    content1 += '<li>Light Armor</li>';
+    content1 += '<li></li><li></li></ul></div></div>';
+    content1 += '<h5> Aryansport Breastplate </h5>';
+    content1 += '<p> 450gp </p>';
+
+    $("#1").html(content1);
+
+    var content2 = '<div class="img-holder">';
+    content2 += '<img src="../img/Armor/fullplateMan.png" id="img2">';
+    content2 += '<div class="hidden text-center" id="hidden2">';
+    content2 += '<ul>';
+    content2 += '<li><b>AC:</b> 18</li>';
+    content2 += '<li>Heavy Armor</li>';
+    content2 += '<li><b>Stealth Disadvantage!</b></li>';
+    content2 += '<li>Strength Requirement: 15</li>';
+    content2 += '<li>Resistance to nacrotic damage</li>';
+    content2 += '</ul></div></div>';
+    content2 += '<h5> Elven Plate Armor </h5>';
+    content2 += '<p> 1,600gp</p>';
+
+    $("#2").html(content2);
+}
